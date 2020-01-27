dd96ae13d1ead03173272512b538fcfdf76d1a67
Jeferson Calazans <calazans10@gmail.com>
2020-01-27 11:37:14 -0300

    Iniciando implementação do loading

--- src/App.js
+++ src/App.js
@@ -1,67 +1,66 @@
-import React, { Component } from 'react';
-import './App.css';
-import Search from './components/Search.js';
-import Table from './components/Table.js';
-import Button from './components/Button.js';
-import fetch from 'isomorphic-fetch';
+import React, { Component } from "react";
+import "./App.css";
+import Search from "./components/Search.js";
+import Table from "./components/Table.js";
+import Button from "./components/Button.js";
+import fetch from "isomorphic-fetch";
 
-  const list = [];
+const list = [];
 
-  const DEFAULT_QUERY = 'redux';
-  const DEFAULT_HPP = '100';
+const DEFAULT_QUERY = "redux";
+const DEFAULT_HPP = "100";
 
-  const PATH_BASE = 'https://hn.algolia.com/api/v1';
-  const PATH_SEARCH = '/search';
-  const PARAM_SEARCH = 'query=';
-  const PARAM_PAGE = 'page=';
-  const PARAM_HPP = 'hitsPerPage=';
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
+const PARAM_PAGE = "page=";
+const PARAM_HPP = "hitsPerPage=";
 
 class App extends Component {
+  state = {
+    results: null,
+    searchKey: "",
+    list,
+    error: null,
+    searchTerm: DEFAULT_QUERY,
+    page: 0,
+    isLoading: false
+  };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      results: null,
-      searchKey: '',
-      list,
-      error: null,
-      searchTerm: DEFAULT_QUERY,
-      page: 0
-    };
-
-    this.onDismiss = this.onDismiss.bind(this);
-    this.onSearchChange = this.onSearchChange.bind(this);
-    this.setSearchTopStories = this.setSearchTopStories.bind(this);
-    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
-    this.onSearchSubmit = this.onSearchSubmit.bind(this);
-  }
-
-  setSearchTopStories = (result) => {
+  setSearchTopStories = result => {
     const { hits, page } = result;
     const { results, searchKey } = this.state;
 
-    const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
 
-    const updatedHits = [
-      ...oldHits,
-      ...hits
-    ];
+    const updatedHits = [...oldHits, ...hits];
+    const updatedResults = {
+      ...results,
+      [searchKey]: { hits: updatedHits, page }
+    };
+    const updatedList = updatedResults[searchKey].hits;
 
     this.setState({
-      results: { ...results, [searchKey]: {hits: updatedHits, page}}
+      ...this.state,
+      results: updatedResults,
+      list: updatedList,
+      isLoading: false
     });
-
-    this.setState({list: results[searchKey].hits})
-  }
+  };
 
   fetchSearchTopStories = (searchTerm, page = 0) => {
-    console.log(`fetching ${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+    this.setState({ isLoading: true });
+    console.log(
+      `fetching ${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`
+    );
+    fetch(
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
+    )
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(e => this.setState({error: e}));
-  }
+      .catch(e => this.setState({ error: e }));
+  };
 
   componentDidMount() {
     const { searchTerm } = this.state;
@@ -69,39 +68,72 @@ class App extends Component {
     this.setState({ searchKey: searchTerm });
   }
 
-  onDismiss = (id) => {
-    console.log('starting onDismiss...')
-    const {results, searchKey} = this.state;
-    const {hits, page} = results[searchKey];
+  onDismiss = id => {
+    console.log("starting onDismiss...");
+    const { results, searchKey } = this.state;
+    const { hits, page } = results[searchKey];
     const updatedList = hits.filter(item => item.objectID !== id);
-    this.setState({ 
-      results: {...results, [searchKey]: { hits: updatedList, page}}
+    this.setState({
+      results: { ...results, [searchKey]: { hits: updatedList, page } }
     });
-  }
+  };
 
-  onSearchChange = (textoBusca) => {
+  onSearchChange = textoBusca => {
     this.setState({ searchTerm: textoBusca.target.value });
-  }
+  };
 
-  onSearchSubmit = (event) => {
+  onSearchSubmit = event => {
     const { searchTerm } = this.state;
-    this.setState({searchKey: searchTerm});
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
     event.preventDefault();
-  }
+  };
 
   render() {
-    const { searchTerm, results, searchKey, error } = this.state;
-    const page = (results && results[searchKey] && results[searchKey].page) || 0;
+    const {
+      searchTerm,
+      results,
+      list,
+      searchKey,
+      isLoading,
+      error
+    } = this.state;
+    const page =
+      (results && results[searchKey] && results[searchKey].page) || 0;
+    const errorMessage = error?.error ?? "";
 
     return (
       <div className="page">
         <div className="interactions">
-          <Search searchTerm={searchTerm} onSearchSubmit={this.onSearchSubmit} onSearchChange={this.onSearchChange}> Search </Search>
+          <Search
+            searchTerm={searchTerm}
+            onSearchSubmit={this.onSearchSubmit}
+            onSearchChange={this.onSearchChange}
+          >
+            {" "}
+            Search{" "}
+          </Search>
         </div>
-        {results && error ? <Table list={results[searchKey].hits} searchTerm={searchTerm} onDismiss={this.onDismiss}/> : <h3>Something went wrong.</h3>}
+        {errorMessage === "" ? (
+          <Table
+            list={list}
+            searchTerm={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        ) : (
+          <h3>Something went wrong.</h3>
+        )}
         <div className="interactions">
-          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}> More </Button>
+          {isLoading ? (
+            <div>Loading...</div>
+          ) : (
+            <Button
+              onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+            >
+              {" "}
+              More{" "}
+            </Button>
+          )}
         </div>
       </div>
     );
@@ -110,4 +142,4 @@ class App extends Component {
 
 export default App;
 
-export { Button, Search, Table};
+export { Button, Search, Table };
